6cc609984dfec14ee0640496865cd644aeafb877
Ken Lauer <klauer@slac.stanford.edu>
2022-09-01 13:55:25 -0700

    Initial commit from lightpath PR

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source ~/virtualenv/python3.9/bin/activate
+
+python --version
+
+pip install -v --upgrade pip
+echo "Req File':' ${REQUIREMENTS:=requirements.txt}"
+echo "Dev Req File':' ${DEV_REQUIREMENTS:=dev-requirements.txt}"
+echo "Pip packages installed for CI':' ${PIP_CI_PACKAGES:=pytest-cov}"
+
+# Install requirements
+if [[ ! -f "${REQUIREMENTS}" ]]; then
+    echo "File not found: ${REQUIREMENTS}" 1>&2
+    travis_terminate 1
+else
+    pip install -v --requirement "${REQUIREMENTS}"
+fi
+
+# Install development requirements
+if [[ ! -f "${DEV_REQUIREMENTS}" ]]; then
+    echo "File not found: ${DEV_REQUIREMENTS}" 1>&2
+    travis_terminate 1
+else
+    pip install -v --requirement "${DEV_REQUIREMENTS}"
+fi
+
+# Install Extras such as PyQt5
+if [[ ! -z "${PIP_EXTRAS}" ]]; then
+    echo "Installing extra pip dependencies."
+    pip install -v ${PIP_EXTRAS}
+fi
+
+# Install Extras such as PyQt5
+if [[ ! -z "${PIP_CI_PACKAGES}" ]]; then
+    echo "Installing pip dependencies for CI."
+    pip install -v ${PIP_CI_PACKAGES}
+fi
+
---
+++ test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -x
+
+source ~/virtualenv/python3.9/bin/activate
+
+git pull
+
+python --version
+
+export DISPLAY=:99
+
+# sudo systemctl start xvfb || exit 1
+/usr/bin/Xvfb ${DISPLAY} -screen 0 1024x768x24 &
+sleep 1
+herbstluftwm &
+sleep 1
+
+AFTER_FAILURE_LOGFILE="${AFTER_FAILURE_LOGFILE:-logs/run_tests_log.txt}"
+
+PYTEST_ARGS=(-v)
+PYTEST_ARGS+=(--cov=.)
+PYTEST_ARGS+=(--log-file="${AFTER_FAILURE_LOGFILE}")
+PYTEST_ARGS+=(--log-format='%(asctime)s.%(msecs)03d %(module)-15s %(levelname)-8s %(threadName)-10s %(message)s')
+PYTEST_ARGS+=(--log-file-date-format='%H:%M:%S')
+PYTEST_ARGS+=(--log-level=DEBUG)
+
+ulimit -c unlimited
+
+pytest "${PYTEST_ARGS[@]}"
+
+if [ $? -eq 0 ]; then
+  echo "Successful run"
+else
+  LOGFILE="${AFTER_FAILURE_LOGFILE:-logs/run_tests_log.txt}"
+  if [ -f "${LOGFILE}" ]; then
+    cat "${LOGFILE}"
+  else
+    echo "Logfile ${LOGFILE} not found"
+  fi
+fi
+
+echo "add-auto-load-safe-path /opt/python/3.9.6/bin/python3.9-gdb.py" >> ~/.gdbinit
+
+export PYTHON_BIN="$(python -c 'import sys; print(sys.executable)')"
+
+if [ -f core ]; then
+  gdb "$PYTHON_BIN" core
+fi
+
+/bin/bash --login
